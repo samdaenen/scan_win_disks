ddc3981a8808ab9e8bae9ca8664f2d4a2034932a
Sam Daenen <sam.daenen@continu-it.be>
2019-07-31 15:23:35 +0200

    Added the ability to use arguments and to call help feature

--- scan_win_disks.py
+++ scan_win_disks.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python
 import re
 import subprocess
 import json
+import argparse
 
-hostlist = raw_input("Location of the hostlist: ")
-output = raw_input("Where should i place the export file?")
+parser = argparse.ArgumentParser(description='Scan Windows host for existing disks')
+parser.add_argument('-s','--source',
+                    help="Source file",
+                    default=None,
+                    dest="source"
+                    )
+parser.add_argument('-o','--output',
+                    help="Destination file",
+                    default=None,
+                    dest="output"
+                    )
+args = parser.parse_args()
+
 host_array = []
 host_disks = {}
-listing = {}
+count= 0
 
-with open(hostlist, 'r') as f:
+with open(args.source, 'r') as f:
         host = f.read().splitlines()
         host_array = host
 
@@ -19,5 +32,5 @@ for i in host_array:
         stdout, stderr = result.communicate()
         regex = re.compile(r'([A-Z]):\\ used\'=(\d+\.?\d*\w*;?){5}')
         host_disks[i] = [ d[0] for d in  regex.findall(stdout.strip())]
-        with open(output, 'w') as json_file:
+        with open(args.output, 'w') as json_file:
                 json_file.write(json.dumps([host_disks]))
